8c867a6b7d38690f5e829dc12c987d638abaa048
jcymeow <20970736@qq.com>
2025-12-11 20:26:00 +0800

    v1.3.4
    封版

--- jcy/jcy_constants.py
+++ jcy/jcy_constants.py
@@ -29,10 +29,10 @@ LANG = None
 APP_NAME = "jcy控制器"
 
 # MOD版本
-APP_VERSION = "v1.3.3"
+APP_VERSION = "v1.3.4"
 
 # 发布日期
-APP_DATE = "20251208"
+APP_DATE = "20251211"
 
 # 控制器全称
 APP_FULL_NAME = f"{APP_NAME}_{APP_VERSION}"
